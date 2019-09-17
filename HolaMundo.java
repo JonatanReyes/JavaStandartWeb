195693a17cc97a20ace94fc5414ecff4249a05da
Jonatan Reyes <54383231+JonatanReyes@users.noreply.github.com>
2019-09-17 12:52:48 -0300

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,49 @@
+package hm;
+
+import javax.swing.*;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        /* int x = 10;
+        
+         if (x > 0)
+         {
+         System.out.println("Positivo");
+         }
+         else
+         if (x < 0)
+         System.out.println("Negativo");
+         else
+         System.out.println("Cero");
+        
+         int n1, n2, n3;
+         String num1, num2, num3;
+         float promedio;
+        
+         num1 = JOptionPane.showInputDialog("Ingrese el primer numero:");
+         num2 = JOptionPane.showInputDialog("Ingrese el segundo numero:");
+         num3 = JOptionPane.showInputDialog("Ingrese el tercer numero:");
+        
+         n1 = Integer.parseInt(num1);
+         n2 = Integer.parseInt(num2);
+         n3 = Integer.parseInt(num3);
+        
+         promedio = (((float)n1+n2+n3)/3);
+        
+         JOptionPane.showMessageDialog( null, "El promedio es: " + promedio);
+         
+
+        int i;
+
+        for (i = 1; i <= 20; i++)
+        {
+            if ((i != 13) && (i != 17))
+            System.out.println(i + " Hola Mundo");
+        }
+                */
+        
+        
+    }
+
+}
